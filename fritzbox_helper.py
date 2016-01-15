51c0ec4b9fb233c95f9400e7a6284c0ac014225b
Eric D <eric13710@gmail.com>
2016-01-15 23:04:02 +0100

    user password improvement

--- fritzbox_helper.py
+++ fritzbox_helper.py
@@ -25,7 +25,7 @@ from xml.dom import minidom
 USER_AGENT = "Mozilla/5.0 (U; Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0"
 
 
-def get_sid(server, password, port=80):
+def get_sid(server, password, username='', port=80):
     """Obtains the sid after login into the fritzbox"""
     conn = httplib.HTTPConnection(server + ':' + str(port))
 
@@ -59,6 +59,8 @@ def get_sid(server, password, port=80):
                "User-Agent": USER_AGENT}
 
     login_page = "/login_sid.lua?&response=" + response_bf
+    if username and not username.isspace():
+        login_page +='&username='+username
     conn.request("GET", login_page, '', headers)
     response = conn.getresponse()
     data = response.read()
@@ -91,3 +93,4 @@ def get_page(server, sid, page, port=80):
         sys.exit(0)
     else:
         return data
+
